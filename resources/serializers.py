bdc260703094891218b0d764a147856c57fc6d43
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-08-24 15:43:37 -0700

    Add resource submission and approval workflow

--- resources/serializers.py
+++ resources/serializers.py
@@ -1,21 +1,11 @@
 from rest_framework import serializers
-from .models import Category, Resources,Tag
+from .models import Category, Resources,Tag, ResourceSubmission
 
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
         fields = ['id', 'name', 'description']
-        
-        
-# class ResourcesSerializer(serializers.ModelSerializer):
-    
-#     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-#     category_name = serializers.StringRelatedField(source='category', read_only=True) 
-#     uploaded_by = serializers.StringRelatedField(read_only=True)
-#     class Meta:
-#         model = Resources
-#         fields = "__all__"
 
 class ResourcesSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
@@ -35,3 +25,39 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['id', 'name']
+
+
+class ResourceSubmissionSerializer(serializers.ModelSerializer):
+    category_name = serializers.StringRelatedField(
+        source="category",
+        read_only=True
+    )
+
+    submitted_by = serializers.StringRelatedField(read_only=True)
+    approved_by = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = ResourceSubmission
+        fields = [
+            "id",
+            "resource_type",
+            "title",
+            "description",
+            "file",
+            "link",
+            "category",
+            "category_name",
+            "submitted_by",
+            "status",
+            "approved_by",
+            "submitted_at",
+            "approved_at",
+        ]
+
+        read_only_fields = [
+            "submitted_by",
+            "status",
+            "approved_by",
+            "submitted_at",
+            "approved_at",
+        ]
