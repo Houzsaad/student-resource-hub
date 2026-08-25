5137dd04c910efc92c94f745e65bdca4069ce270
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-08-24 17:15:20 -0700

    Add resource submission approval workflow

--- resources/admin.py
+++ resources/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Category, Resources, Tag
+from .models import Category, Resources, Tag, ResourceSubmission
 
 admin.site.register(Category)
 
 admin.site.register(Resources)
 
 admin.site.register(Tag)
+
+admin.site.register(ResourceSubmission)
 
 # Register your models here.
