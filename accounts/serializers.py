36ef737701f0ccdf13a9f1d83e285074d14140df
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-09-04 16:52:11 -0700

    Fix HEAD bug which corse by merging git  branch

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -9,12 +9,8 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-<<<<<<< HEAD
-        fields = ['id', 'email', 'full_name', 'password', 'role', 'department', 'level']
-=======
         fields = ['id', 'email', 'full_name', 'password', 'faculty', 'department', 'role']
         read_only_fields = ['id', 'date_joined', 'role']
->>>>>>> 5b81de312c97f8c81c31149a7945452743572f2c
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
@@ -24,10 +20,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-<<<<<<< HEAD
         fields = ['id', 'full_name', 'role', 'role', 'department', 'level', 'date_joined']
         read_only = ['id', 'date_joined']
-=======
-        fields = ['id', 'full_name', 'role', 'faculty', 'department', 'date_joined']
-        read_only_fields = ['id', 'date_joined', 'role']
->>>>>>> 5b81de312c97f8c81c31149a7945452743572f2c
