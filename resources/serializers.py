a54c76e6e93f3893e3ce6cc9fd8713e09bebad57
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-09-08 01:34:48 -0700

    update resource serializer, to supprt pdf review

--- resources/serializers.py
+++ resources/serializers.py
@@ -7,11 +7,26 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id', 'name', 'description']
 
+# class ResourcesSerializer(serializers.ModelSerializer):
+#     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+#     category_name = serializers.StringRelatedField(source='category', read_only=True)
+#     uploaded_by = serializers.StringRelatedField(read_only=True)
+#     is_owner = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = Resources
+#         fields = "__all__"
+
+#     def get_is_owner(self, obj):
+#         request = self.context.get('request')
+#         return bool(request and request.user.is_authenticated and obj.uploaded_by == request.user)
+
 class ResourcesSerializer(serializers.ModelSerializer):
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     category_name = serializers.StringRelatedField(source='category', read_only=True)
     uploaded_by = serializers.StringRelatedField(read_only=True)
     is_owner = serializers.SerializerMethodField()
+    file = serializers.SerializerMethodField()
 
     class Meta:
         model = Resources
@@ -20,6 +35,14 @@ class ResourcesSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context.get('request')
         return bool(request and request.user.is_authenticated and obj.uploaded_by == request.user)
+
+    def get_file(self, obj):
+        if not obj.file:
+            return None
+        url = obj.file.url
+        if obj.resource_type == Resources.ResourceType.PDF:
+            url = url.replace('/raw/upload/', '/image/upload/')
+        return url
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
